aac608791a67eb07ae647b6f085b28d69d9c1029
Dylan J <dylan@tritan.dev>
2023-02-13 07:19:32 -0500

    comments bc idk if im gonna understand this tomorrow

--- proxy.py
+++ proxy.py
@@ -1,18 +1,18 @@
 import socket
 import time
 
-target_host = "localhost"
-target_ports = [8080]
-proxy_port = 8069
+target_host = "localhost"  # server hostname to load balance to
+target_ports = [8080, 8081, 8082]  # server ports to load balance to
+proxy_port = 8069  # proxy port to listen on
 
-rate_limit = 10
-buffer_size = 4096
+rate_limit = 10  # seconds
+buffer_size = 4096 # bytes
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.bind(("", proxy_port))
 listen_socket.listen(5)
 
-request_count = 0
+request_count = 0 
 last_request_time = time.time()
 
 while True:
@@ -29,7 +29,7 @@ while True:
         client_socket.close()
         continue
 
-    target_port = target_ports[0]
+    target_port = target_ports[request_count % len(target_ports)]
 
     target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     target_socket.connect((target_host, target_port))
